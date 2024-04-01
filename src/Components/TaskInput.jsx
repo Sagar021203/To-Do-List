232416eb8dc4489ea78041d7142f834fb198e713
Sagarr <150433339+Sagar021203@users.noreply.github.com>
2024-04-02 04:09:43 +0530

    Update TaskInput.jsx

--- src/Components/TaskInput.jsx
+++ src/Components/TaskInput.jsx
@@ -3,19 +3,25 @@ import React, { useState,useEffect } from 'react';
 import './To-Do-List.css';
 
 function TaskInput({ onAddTask }) {
+ // State to store the new task input
   const [newTask, setNewTask] = useState("");
+ // useEffect hook to log a message when the component mounts
   useEffect(() => {
     console.log("TaskInput mounted!");
 
   }, []);
-
+// Handler function to update the newTask state when input changes
   function handleInputChange(e) {
     setNewTask(e.target.value);
   }
-
+// Handler function to add a new task when the 'ADD' button is clicked
   function handleAddTask() {
+   // Check if the new task input is not empty
     if (newTask.trim() !== "") {
+      // Call the onAddTask function passed from the parent component
+      // to add the new task
       onAddTask(newTask);
+     // Clear the newTask state after adding the task
       setNewTask("");
     }
   }
